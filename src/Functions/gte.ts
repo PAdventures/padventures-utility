3e314a2a7d5809cf5b604fb35111ef41bb29a13e
PAdventures <sdulyea@icloud.com>
2024-05-14 17:43:31 +0100

    Feat: Simple math functions added

---
+++ src/Functions/gte.ts
@@ -0,0 +1,8 @@
+/**
+ * @param num1 The number to compare
+ * @param num2 The other number to compare
+ * @returns {boolean} Returns `true` if `num1` is greater than or equal to `num2`, and `false` if it's not
+ */
+export default function gte(num1: number, num2: number): boolean {
+    return num1 >= num2;
+}
